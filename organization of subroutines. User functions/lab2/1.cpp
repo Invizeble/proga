3b29e0b30584f502dd904b5afd488d3500260ce4
Invizeble <161184827+Invizeble@users.noreply.github.com>
2024-10-14 08:12:51 +0600

    Add files via upload

---
+++ organization of subroutines. User functions/lab2/1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double max(double x, double y) {
+
+    if (x > y) {
+        return x;
+    }
+    else {
+        return y;
+    }
+}
+
+
+int main() {
+
+    double a, b, sum;
+
+    cout << "Enter a and b ";
+    cin >> a;
+    cin.ignore(1, ',');
+    cin >> b;
+
+    sum = max(a, 2 * b) + max(2 * a - b, b);
+
+	cout << "sum is: " << sum;
+
+}
---
+++ organization of subroutines. User functions/lab2/3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+int simp(int number) {
+	if (number < 10 || number >= 100) {
+		return -1; 
+	}
+
+	for (int i = 2; i <= sqrt(number); i++) {
+		if (number % i == 0) {
+			return -1;
+		}
+	}
+	return number;
+}
+
+
+
+int main() {
+	srand(time(0));
+
+	list<int> my_List;
+
+	for (int i = 0; i < 10; ++i) {
+
+		int randomnumber = rand() % 101;
+		my_List.push_back(randomnumber);
+
+	}
+
+	for(int number : my_List) {
+		if (simp(number) != -1) {
+			cout << number << " - simple" << endl;
+		}
+		else {
+			cout << number << " - not simple or not in range from 10 to 99" << endl;
+		}
+	}
+
+
+}
