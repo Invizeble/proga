f37225e1ce0eda18af9877a85da275232a792629
invize <invize@yahoo.com>
2024-11-17 20:28:49 +0500

    Initial commit

---
+++ Java/Main.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 1;
+        int num1 = 2;
+        int sum = num + num1;
+
+        System.out.println(sum);
+    }
+}
